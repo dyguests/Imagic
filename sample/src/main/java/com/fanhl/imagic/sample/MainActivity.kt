64120b02077dabb9dcdc1d9ff44647458c39bc72
fanhl <fanhonglin@klicen.com>
2018-09-07 14:07:42 +0800

    bytes

--- sample/src/main/java/com/fanhl/imagic/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/imagic/sample/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.fanhl.imagic.sample
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import java.nio.ByteBuffer
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawable = getDrawable(R.mipmap.ic_launcher)
+        val bitmapDrawable = drawable as? BitmapDrawable ?: return
+        val bitmap = bitmapDrawable.bitmap
+        val byteBuffer = ByteBuffer.allocate(bitmap.byteCount).also {
+            bitmap.copyPixelsToBuffer(it)
+        }
+
+        Log.d(TAG, "onCreate: ")
     }
 }
